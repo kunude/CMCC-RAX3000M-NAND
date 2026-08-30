b7f8bac473a121139ffb87ffeb2275634ba749d2
kunude <85596880+kunude@users.noreply.github.com>
2026-08-30 08:47:03 +0800

    Modify IP, remove outdated packages, and clone Passwall
    Updated default IP and removed outdated luci version. Cloned necessary packages for Passwall.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,26 +1,16 @@
-#!/bin/bash
+# Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+# sed -i 's/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/CMCCRax3000M-Router/g' package/base-files/files/bin/config_generate
 
-# 移除 feeds 自带的核心库
+# 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
+# 移除 openwrt feeds 过时的luci版本
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
-
-# ===== 修复 sing-box 依赖版本 =====
-SING_BOX_MK="package/passwall-packages/sing-box/Makefile"
-sed -i '/^define Build\/Prepare/,/^endef/d' "$SING_BOX_MK"
-cat >> "$SING_BOX_MK" << 'EOF'
-
-define Build/Prepare
-	$(Build/Prepare/Default)
-	( cd $(PKG_BUILD_DIR) && \
-	  go mod edit -replace=github.com/go-json-experiment/json@v0.0.0-20250813024750-ebf49471dced=github.com/go-json-experiment/json@v0.0.0-20250113025959-68c5390da787 && \
-	  go mod download github.com/go-json-experiment/json )
-endef
-EOF
-# ============================================================================
