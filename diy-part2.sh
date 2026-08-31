b057290004e54a6c272aeabf63cf6bee72c22cdd
kunude <85596880+kunude@users.noreply.github.com>
2026-08-31 09:59:23 +0800

    Comment out hostname modification and cleanup
    Comment out hostname modification and retain the previous line for clarity.

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,11 +6,3 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 #sed -i 's/OpenWrt/CMCCRax3000M-Router/g' package/base-files/files/bin/config_generate
-
-# 移除 openwrt feeds 自带的核心库
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-
-# 移除 openwrt feeds 过时的luci版本
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
